f76b400909cd99157445cf0ac562d928388c9aaf
MarcusKLY <50311376+MarcusKLY@users.noreply.github.com>
2026-03-23 14:59:32 +0200

    Initial Commit

---
+++ MyPhoto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPhoto
+//
+//  Created by Kam Long Yin on 23/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPhoto/MyPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPhotoApp.swift
+//  MyPhoto
+//
+//  Created by Kam Long Yin on 23/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
